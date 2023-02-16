8ff20f9586efcc1d20fe72885e32ed9a79a9788e
David Braden <david.braden@yum.com>
2023-02-16 21:07:05 +0000

    Update ranking

--- src/main.ts
+++ src/main.ts
@@ -19,6 +19,11 @@ const PLAYERS: Player[] = [
         steam_id: '76561198220073969',
     },
     {
+        name: 'RAD',
+        profile_id: '2418734',
+        steam_id: '76561197993028790',
+    },
+    {
         name: 'waluwewe',
         profile_id: '1533683',
         steam_id: '76561198058125099',
@@ -27,11 +32,6 @@ const PLAYERS: Player[] = [
         name: 'Simie',
         profile_id: '1512338',
         steam_id: '76561197988289178',
-    },
-    {
-        name: 'RAD',
-        profile_id: '2418734',
-        steam_id: '76561197993028790',
     },
     {
         name: 'David',
